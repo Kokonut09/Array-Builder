0a6a001f9c4bd59f84935d51521b53a760dff58d
Kokonut09 <a09saeyang@gmail.com>
2020-02-20 23:33:06 -0800

    Array prints correctly. WIP adding user input, range, random numbers

--- Array Builder/src/Arrbees.java
+++ Array Builder/src/Arrbees.java
@@ -2,8 +2,30 @@
 public class Arrbees {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//System.out.println("");
+		getArbees();
 
+	}
+
+	private static void getArbees() {
+		
+		String arbee ="int[] arr = {";
+		int L = 5;
+		
+		for(int i = 0; i<L; i++ ) {
+			
+			if(i == L-1) {
+				arbee += i;
+			}else {
+				arbee += i + ", ";
+			}
+			
+		}
+		
+		arbee += "};";
+		System.out.println(arbee);
+		
 	}
 
 }
